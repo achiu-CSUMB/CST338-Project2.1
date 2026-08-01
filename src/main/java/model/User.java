2977e957a0ff443c0a719f179efe31b7be98546c
john ly <koopatroopa864@yahoo.com>
2026-08-01 16:24:55 -0700

    forgot constructor for new users

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -19,6 +19,12 @@ public class User {
         this.role = role;
     }
 
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
     public int getUserId() {
         return userId;
     }
