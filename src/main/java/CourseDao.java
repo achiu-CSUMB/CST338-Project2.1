9cdffe5a6df81a8041de4800ac0752ea24591d71
dcasoliprettyawesome <dcasoli@csumb.edu>
2026-08-03 03:49:08 -0700

    Created the skeleton for course and enrollment slice. (#18)

---
+++ src/main/java/CourseDao.java
@@ -0,0 +1,58 @@
+/**
+ * @author Dominic Casoli
+ * <br>
+ * created: 8/2/2026
+ * @since '1.0-SNAPSHOT'
+ * Description:
+ */
+public class CourseDao {
+    /**
+     * Creates a CourseDao object.
+     */
+    public CourseDao() {
+        // TODO: Connect to the project database.
+    }
+
+    /**
+     * Inserts a course to the database.
+     */
+    public boolean insert(Course course) {
+        // TODO: Insert the course into the database.
+        return false;
+    }
+
+    /**
+     * Finds a course through its ID.
+     */
+    public Course findById(int courseId) {
+        // TODO: Retrieve course via the given ID.
+
+        return null;
+    }
+
+    /**
+     * Updates an existing course.
+     */
+    public boolean update(Course course) {
+        // TODO: Update the selected course.
+
+        return false;
+    }
+
+    /**
+     * Deletes a course.
+     */
+    public boolean delete(int courseId) {
+        // TODO: Delete the selected course.
+
+        return false;
+    }
+
+    /**
+     * Returns all the courses.
+     */
+    public void getAllCourses() {
+        // TODO: Retrieve every course from database.
+
+    }
+}
