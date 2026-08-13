b594d7d28e91af5a03bab72ceed810c834cd80c2
john ly <koopatroopa864@yahoo.com>
2026-08-13 13:32:52 -0700

    added back button for acct and fix DB for teacher prefix to write

--- src/main/java/controller/AccountsController.java
+++ src/main/java/controller/AccountsController.java
@@ -1,11 +1,17 @@
 package controller;
 
 import dao.UserDao;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.User;
 
 /**
@@ -180,6 +186,26 @@ public class AccountsController {
 
         if (!currentUser.getRole().equals("Admin")) {
             roleComboBox.setDisable(true);
+        }
+    }
+
+    @FXML
+    private void handleBack(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/views/menu-view.fxml")
+            );
+
+            Parent root = loader.load();
+
+            MenuController controller = loader.getController();
+            controller.setCurrentUser(currentUser);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/controller/MenuController.java
+++ src/main/java/controller/MenuController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.User;
 
@@ -57,8 +58,23 @@ public class MenuController {
     }
 
     @FXML
-    private void handleLogout() {
+    private void handleLogout(ActionEvent event) {
         // Logic to handle logout
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/views/login-view.fxml")
+            );
+
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource())
+                    .getScene()
+                    .getWindow();
+
+            stage.setScene(new Scene(root));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
