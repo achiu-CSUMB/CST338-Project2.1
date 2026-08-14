691ec8315cd9d1b521382c5c17fe714c1c420f76
dcasoliprettyawesome <dcasoli@csumb.edu>
2026-08-14 02:19:22 -0700

    Update CourseServiceTest.java
    Getting rid of unused import.

--- src/test/java/service/CourseServiceTest.java
+++ src/test/java/service/CourseServiceTest.java
@@ -12,7 +12,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
